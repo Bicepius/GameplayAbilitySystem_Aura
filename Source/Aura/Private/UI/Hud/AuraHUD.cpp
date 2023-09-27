f3c2405c6ff73f138b58ea93dc1f8311639ac2e7
Bicepius <124207845+Bicepius@users.noreply.github.com>
2023-09-27 17:17:44 +0100

    Broadcasting initial values

--- Source/Aura/Private/UI/Hud/AuraHUD.cpp
+++ Source/Aura/Private/UI/Hud/AuraHUD.cpp
@@ -31,8 +31,8 @@ void AAuraHUD::InitOverlay(APlayerController* PC, APlayerState* PS, UAbilitySyst
 	UOverlayWidgetController* WidgetController = GetOverlayWidgetController(WidgetControllerParams);
 
 	OverlayWidget->SetWidgetController(WidgetController);
-
-
+	
+	WidgetController->BroadcastInitialValues();
 	
 	Widget->AddToViewport();
 }
